db01d121f40e846f2339f6bc54488614f586869b
Allan Shih <allan.shih.udn@gmail.com>
2022-05-03 11:26:16 +0800

    build: tuned to work with Flutter and are designed according to the clean architecture

---
+++ lib/src/domain/usecases/get_user_usecase.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import '../entities/user.dart';
+import '../repositories/users_repository.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+class GetUserUseCase
+    extends UseCase<GetUserUseCaseResponse, GetUserUseCaseParams> {
+  final UsersRepository usersRepository;
+  GetUserUseCase(this.usersRepository);
+
+  @override
+  Future<Stream<GetUserUseCaseResponse?>> buildUseCaseStream(
+      GetUserUseCaseParams? params) async {
+    final controller = StreamController<GetUserUseCaseResponse>();
+
+    if (params?.uid != null) {
+      try {
+        // get user
+        final user = await usersRepository.getUser(params!.uid);
+        // Adding it triggers the .onNext() in the `Observer`
+        // It is usually better to wrap the reponse inside a respose object.
+        controller.add(GetUserUseCaseResponse(user));
+        logger.finest('GetUserUseCase successful.');
+        controller.close();
+      } catch (e) {
+        logger.severe('GetUserUseCase unsuccessful.');
+        // Trigger .onError
+        controller.addError(e);
+      }
+    } else {
+      logger.severe('GetUserUseCase unsuccessful. User id is null');
+      // Trigger .onError
+      controller.addError(Exception('User id is null'));
+    }
+    return controller.stream;
+  }
+}
+
+/// Wrapping params inside an object makes it easier to change later
+class GetUserUseCaseParams {
+  final String uid;
+  GetUserUseCaseParams(this.uid);
+}
+
+/// Wrapping response inside an object makes it easier to change later
+class GetUserUseCaseResponse {
+  final User user;
+  GetUserUseCaseResponse(this.user);
+}
