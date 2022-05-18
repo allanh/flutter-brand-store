32d6802a86f1a30afcf4490e770ca3fe93356864
sam_lai_udn <sam.lai@udnshopping.com>
2022-05-18 09:48:36 +0800

    feat: WIP 會員資料

--- lib/src/app/pages/member_center/profile/member_profile_view.dart
+++ lib/src/app/pages/member_center/profile/member_profile_view.dart
@@ -1,5 +1,7 @@
 import 'package:brandstores/src/app/pages/member_center/profile/member_profile_controller.dart';
 import 'package:brandstores/src/data/repositories/data_member_profile_repository.dart';
+import 'package:brandstores/src/device/utils/my_plus_colors.dart';
+import 'package:brandstores/src/domain/entities/member_profile/member_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
@@ -17,17 +19,72 @@ class _MemberProfilePageState
 
   @override
   Widget get view {
-    return ControlledWidgetBuilder<MemberProfileController>(
-      builder: (context, controller) {
-        if (controller.memberProfile != null) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('會員資料'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('會員資料')),
+      body: ControlledWidgetBuilder<MemberProfileController>(
+        builder: (context, controller) {
+          return Container(
+            child: Column(
+              children: [
+                /// 生日修改次數說明
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 12.0, right: 12.0, top: 12.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 48.0,
+                    child: Text('作為系統預設之訂購人資料，請務必填寫正確(生日僅可修改一次)。',
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption
+                            ?.copyWith(color: UdiColors.brownGrey)),
+                  ),
+                ),
+
+                /// 姓名區塊
+                Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0, right: 12.0, top: 16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 80.0,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                  text: '姓名',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .caption
+                                      ?.copyWith(color: UdiColors.greyishBrown),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: "*",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .caption
+                                            ?.copyWith(
+                                                color: UdiColors.strawberry))
+                                  ]),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 16),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Enter a search term',
+                                ),
+                              ),
+                            ),
+                          ]),
+                    )),
+              ],
             ),
           );
-        }
-        return Container();
-      },
+        },
+      ),
     );
   }
 }
--- lib/src/device/utils/my_plus_colors.dart
+++ lib/src/device/utils/my_plus_colors.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 extension UdiColors on Colors {
+  static const Color white = Color(0xfffbfbfb);
   static const Color white2 = Color(0xfff2f2f2);
   static const Color greyishBrown = Color(0xff4c4c4c);
-  static const Color brownGrey = Color(0xff7c7c7c);
+  static const Color brownGrey = Color(0xff7f7f7f);
+  static const Color brownGrey2 = Color(0xffb4b4b4);
+  static const Color strawberry = Color(0xfff23f44);
+  static const Color green = Color(0xff1ab643);
 }
