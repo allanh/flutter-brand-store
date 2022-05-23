f35a3047e4a3dab7823aa50307621eab6165303c
Sam Lai <sam.lai@udnshopping.com>
2022-05-23 12:34:22 +0800

    feat: 儲存按鈕

--- lib/src/app/pages/member_center/profile/member_profile_view.dart
+++ lib/src/app/pages/member_center/profile/member_profile_view.dart
@@ -126,9 +126,32 @@ class _MemberProfilePageState
                 context, controller.memberProfile?.isSettingPassword ?? false,
                 (message) {
               debugPrint(message);
+            }),
+
+            _buildSaveButton(context, () {
+              debugPrint('save button pressed');
             })
           ]);
         }));
+  }
+
+  /// 建立儲存按鈕
+  Padding _buildSaveButton(BuildContext context, void Function() handleSave) {
+    return Padding(
+        padding: const EdgeInsets.only(left: 12.0, right: 12.0, bottom: 12.0),
+        child: SizedBox(
+            width: double.infinity,
+            height: 36.0,
+            child: ElevatedButton(
+                child: const Text('儲存'),
+                onPressed: handleSave,
+                style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).appBarTheme.backgroundColor,
+                    textStyle: const TextStyle(
+                        color: UdiColors.white,
+                        fontFamily: 'PingFangTC Semibold',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.0)))));
   }
 
   /// 建立密碼設定區塊
